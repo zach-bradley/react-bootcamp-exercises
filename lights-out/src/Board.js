187f9a32a5b0d53587f73b437632120c21f156bd
Zach Bradley <zachbradleydev@gmail.com>
2019-12-30 15:30:26 -0500

    updating packages

--- lights-out/src/Board.js
+++ lights-out/src/Board.js
@@ -30,13 +30,18 @@ import './Board.css';
  **/
 
 class Board extends Component {
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightStartsOn: 0.25 
+  };
 
   constructor(props) {
     super(props);
     // TODO: set initial state
     this.state = {
       hasWon : false,
-      board : 
+      board : this.createBoard()
     }
   }
 
@@ -45,6 +50,13 @@ class Board extends Component {
   createBoard() {
     let board = [];
     // TODO: create array-of-arrays of true/false values
+    for(let y=0; y < this.props.nrows; y++) {
+      let row = [];
+      for(let x = 0; x < this.props.ncols; x++) {
+        row.push(Math.random() < this.props.chanceLightStartsOn)
+      }
+      board.push(row);
+    }
     return board
   }
 
@@ -69,7 +81,7 @@ class Board extends Component {
     // win when every cell is turned off
     // TODO: determine is the game has been won
 
-    this.setState({board, hasWon});
+    //this.setState({board, hasWon});
   }
 
 
@@ -80,15 +92,20 @@ class Board extends Component {
     // TODO
     // make table board
     // TODO
-    <table>
+    let tblBoard = [];
+    for(let y= 0; y < this.props.nrows; y++) {
+      let row = [];
+      for(let x = 0; x < this.props.ncols; x++) {
+        row.push(<Cell isLit={this.state.board[y][x]} />)
+      }
+    }
+    return (
+      <table className='Board'>
       <tbody>
-        <tr>
-          <Cell isLit={true} />
-          <Cell isLit={false} />
-          <Cell isLit={true} />
-        </tr>
+        {tblBoard}
       </tbody>
     </table>
+    )
   }
 }
 
